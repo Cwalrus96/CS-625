153be16ed644f2288d222985cd33f06ea48f776f
Cwalrus96 <Caleb.compton@gmail.com>
2017-10-04 17:22:03 -0500

    started arranging code for star distribution

--- hw4-star.c
+++ hw4-star.c
@@ -9,11 +9,7 @@
 #include<sys/resource.h>
 #include<string.h>
 
-//MAIN 
-
-int main(int argc, char** argv) 
-{
-	//1. GLOBAL VARIABLES 
+//1. GLOBAL VARIABLES 
 	char ** wiki; 
 	char ** dict; 
 	int ** globalIndex; 
@@ -33,11 +29,21 @@ int main(int argc, char** argv)
 	int rank; 
 	double elapsedTime; 
 	MPI_Status stat;
-	//2. Initialize MPI
-	MPI_Init(&argc, &argv); 
-	MPI_Comm_size(MPI_COMM_WORLD, &numCores);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
-	//3. All Processes will initialize their arrays 
+
+void initArrays()
+{
+	if(rank == 0) 
+	{
+		initGlobalArrays(); 
+	}
+	else 
+	{
+		initLocalArrays(); 	
+	}
+}
+
+void initGlobalArrays() 
+{
 	wiki = (char **) malloc(numWiki * sizeof(char *));
 	wiki[0] = (char *) malloc(numWiki * lineLength * sizeof(char)); 
 	for(i = 1; i < numWiki; i++) 
@@ -50,38 +56,53 @@ int main(int argc, char** argv)
 	{
 		dict[i] = dict[i - 1] + wordLength; 
 	}
-	//4. Only server will populate it's arrays, then broadcast to all processes 
-	if(rank == 0) 
-	{
-		f = fopen("/homes/cwalrus96/hw3/keywords.txt", "r"); 
-		if(NULL == f) {
-			perror("FAILED: "); 
-			return -1; 
-		} 
-		numWords = 0; 
-		while(err != EOF && numWords < numDict) 
-		{
-			err = fscanf(f, "%[^\n]\n", dict[numWords]); 
-			numWords ++; 
-		}
-		fclose(f); 
-		f = fopen("/homes/cwalrus96/hw3/wiki_dump.txt", "r"); 
-		if(NULL == f) {
-			perror("FAILED: "); 
-			return -1; 
-		}
-		while(err != EOF && numLines < numWiki) 
-		{
-			err = fscanf(f, "%[^\n]\n", wiki[numLines]); 
-			numLines ++; 
-		}
-		fclose(f); 
-		gettimeofday(&t1, NULL); 
+}
 
+void initLocalArrays() 
+{
+	
+}
+
+void popArrays() 
+{
+	f = fopen("/homes/cwalrus96/hw3/keywords.txt", "r"); 
+	if(NULL == f) {
+		perror("FAILED: "); 
+		return -1; 
+	} 
+	numWords = 0; 
+	while(err != EOF && numWords < numDict) 
+	{
+		err = fscanf(f, "%[^\n]\n", dict[numWords]); 
+		numWords ++; 
 	}
-	MPI_Bcast(wiki[0], lineLength * numWiki, MPI_CHAR, 0, MPI_COMM_WORLD);
-	MPI_Bcast(dict[0], wordLength * numDict, MPI_CHAR, 0, MPI_COMM_WORLD);
-	//5. Each array will search a different portion of the dictionary, based on their rank. They will store the indexes in their localIndex array 
+	fclose(f); 
+	f = fopen("/homes/cwalrus96/hw3/wiki_dump.txt", "r"); 
+	if(NULL == f) {
+		perror("FAILED: "); 
+		return -1; 
+	}
+	while(err != EOF && numLines < numWiki) 
+	{
+		err = fscanf(f, "%[^\n]\n", wiki[numLines]); 
+		numLines ++; 
+	}
+	fclose(f); 
+	gettimeofday(&t1, NULL);
+}
+
+void distributeArrays() 
+{
+	
+}
+
+void recieveArrays() 
+{
+	
+}
+
+void searchArrays() 
+{
 	start = (numDict / numCores) * rank; 
 	end = start + (numDict / numCores); 
 	if(rank == numCores - 1) end = numDict; 
@@ -110,63 +131,52 @@ int main(int argc, char** argv)
 				localIndex[numWords - start][0] ++; 
 			}
 		}
-	}
-	//6. All processes will send their temporary arrays (1 line at a time) to the root.
-	if(rank != 0) {
-		for(i = start; i < end; i++) 
-		{	
-			MPI_Send(localIndex[i - start], localIndex[i - start][1], MPI_INT, 0, i, MPI_COMM_WORLD);
-		}
-	}
-	//7. The root will collect these arrays into one large global index
-	if(rank == 0) {
-		globalIndex = (int **) malloc(sizeof(int *) * numDict);
-		for(i = start; i < end; i++) {
-			globalIndex[i] = localIndex[i]; 
-		}
-		for(i = end; i < numDict; i++) 
-		{
-			MPI_Probe(MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &stat); 
-			MPI_Get_count(&stat, MPI_INT, &size); 
-			globalIndex[i] = (int *) malloc(sizeof(int) * size);
-			MPI_Recv(globalIndex[i], size, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &stat); 
-		}
-	}
-	//8. Print out results (master thread only) 
-	if(rank == 0) 
+	}	
+}
+
+void distributeKeys() 
+{
+	
+}	
+
+void printResults() 
+{
+	gettimeofday(&t2, NULL); 
+	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; 
+	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; 
+	getrusage(RUSAGE_SELF, &memUsed); 
+	numWords = 0; 
+	while((numWords < numDict) && !(NULL == dict[numWords]))
 	{
-		gettimeofday(&t2, NULL); 
-		elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; 
-		elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; 
-		getrusage(RUSAGE_SELF, &memUsed); 
-		numWords = 0; 
-		while((numWords < numDict) && !(NULL == dict[numWords]))
-		{
-			if(globalIndex[numWords][0] > 2) {
-				printf("%-10s: ", dict[numWords]); 
-				i = 2; 
-				while(!(NULL == globalIndex[numWords]) && (i < globalIndex[numWords][0]))
-				{
-					printf("%d, ", globalIndex[numWords][i]); 
-					i++; 
-				}
-				printf(" \n");
+		if(globalIndex[numWords][0] > 2) {
+			printf("%-10s: ", dict[numWords]); 
+			i = 2; 
+			while(!(NULL == globalIndex[numWords]) && (i < globalIndex[numWords][0]))
+			{
+				printf("%d, ", globalIndex[numWords][i]); 
+				i++; 
 			}
-			numWords ++;
+			printf(" \n");
 		}
-		int groupSize; 
-		if(argc == 1) {
-			groupSize = numCores;
-		}
-			else groupSize = atoi(argv[1]); 
-		double secs = (elapsedTime/ 1000.0); 
-		int hours = secs / 3600; 
-		secs = secs - (3600 * hours); 
-		int minutes = secs/60;
-		secs = secs - (60 * minutes);  
-		printf("DATA, %d, %d, %d:%d:%f, %ld, \n", numCores,groupSize, hours, minutes, secs, memUsed.ru_maxrss); 
+		numWords ++;
 	}
-	//9. Free all arrays and finalize mpi processes 
+	int groupSize; 
+	if(argc == 1) {
+		groupSize = numCores;
+	}
+		else groupSize = atoi(argv[1]); 
+	double secs = (elapsedTime/ 1000.0); 
+	int hours = secs / 3600; 
+	secs = secs - (3600 * hours); 
+	int minutes = secs/60;
+	secs = secs - (60 * minutes);  
+	printf("DATA, %d, %d, %d:%d:%f, %ld, \n", numCores,groupSize, hours, minutes, secs, memUsed.ru_maxrss); 
+
+	
+}
+
+void freeAll() 
+{
 	free(dict[0]); 
 	free(dict); 
 	for(numWords = 0; numWords < range; numWords ++) 
@@ -184,6 +194,50 @@ int main(int argc, char** argv)
 	}
 	free(wiki[0]); 
 	free(wiki); 
-	MPI_Finalize(); 
+	MPI_Finalize();	
+}
+
+
+//MAIN 
+int main(int argc, char** argv) 
+{
+	
+	//2. Initialize MPI (done)
+	MPI_Init(&argc, &argv); 
+	MPI_Comm_size(MPI_COMM_WORLD, &numCores);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+	//3. All Processes will initialize their arrays
+		//Server will initialize global arrays, clients will initialize local arrays 
+	initArrays(); 	
+	//4. Only server will populate it's arrays 
+	if(rank == 0) 
+	{	 
+		popArrays(); 
+	}
+	//5. Server will distribute parts of the wiki to all the other threads, based on their rank. (EDIT) 
+	if(rank == 0) {
+		distributeArrays(); 
+	}
+	else 
+	{
+		recieveArrays(); 	
+	}
+	//6. Each array (besides the server) will recieve keywords from the server (1 at a time)
+	//and search their portion of the wiki for that word (up to 100 instances). 
+	if(rank != 0) {
+		searchArrays(); 
+	}
+	//7. The server will listen to the other threads and hand out keywords one at a time. 
+	if(rank == 0) 
+	{
+		distributeKeys(); 	
+	}
+	//8. Print out results (master thread only) 
+	if(rank == 0) 
+	{
+		printResults(); 
+	}
+	//9. Free all arrays and finalize mpi processes 
+		freeAll();  
 	return 0; 
 }
