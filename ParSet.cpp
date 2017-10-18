8e096e28ebb8de2c9266abedc43b6e9846e3caac
Cwalrus96 <Caleb.compton@gmail.com>
2017-10-18 10:24:43 -0500

    Create ParSet.cpp

---
+++ ParSet.cpp
@@ -0,0 +1,24 @@
+#include "ParSet.h" 
+#include <cstdlib>
+
+ParSet::ParSet() {
+  c = RandomizeParameters(30611); 
+  d = RandomizeParameters(113.064); 
+  h = RandomizeParameters(0.5289); 
+  beta = RandomizeParameters(0.0426); 
+  n = RandomizeParameters(0.5916); 
+  lambda2 = RandomizeParameters(1.3484); 
+  b = RandomizeParameters(80.5297); 
+  r = RandomizeParameters(2.85); 
+  s = RandomizeParameters(3.25); 
+  lambda1 = RandomizeParameters(2.9699); 
+  a = RandomizeParameters(1017.1); 
+}
+
+ParSet::RandomizeParameters(float p) {
+  float seed = rand(); 
+  float sign = (seed % 2 == 0) ? -1.0 : 1.0; 
+  seed = seed / RAND_MAX; 
+  float change = seed * 25.0 * sign; 
+  return (p + change); 
+}
