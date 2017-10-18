67a023074bbd6970dce5cee8d15648a17fdd6132
Cwalrus96 <Caleb.compton@gmail.com>
2017-10-18 10:10:55 -0500

    ParSet.cpp

---
+++ ParSet.h
@@ -0,0 +1,14 @@
+class ParSet {
+  public: 
+  float c; 
+  float d; 
+  float h; 
+  float beta; 
+  float lambda2; 
+  float b; 
+  float r; 
+  float s; 
+  float lambda1; 
+  float a; 
+  ParSet(); 
+}
