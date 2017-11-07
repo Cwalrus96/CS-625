e0be831c0dbd4bdd160e8beed2d2400453475c75
Cwalrus96 <Caleb.compton@gmail.com>
2017-11-06 19:06:19 -0600

    Create ParSet.c

---
+++ ParSet.c
@@ -0,0 +1,24 @@
+#include "ParSet.h"
+
+
+initializeParSet(*ParSet p) {
+  p->c = RandomizeParameter(30611); 
+  p->d = RandomizeParameter(113.064); 
+  p->h = RandomizeParameter(0.5289); 
+  p->beta = RandomizeParameter(0.0426); 
+  p->n = RandomizeParameter(0.5916); 
+  p->lambda2 = RandomizeParameter(1.3484); 
+  p->b = RandomizeParameter(80.5297); 
+  p->r = RandomizeParameter(2.85); 
+  p->s = RandomizeParameter(3.25); 
+  p->lambda1 = RandomizeParameter(2.9699); 
+  p->a = RandomizeParameter(1017.1); 
+}
+
+randomizeParameter(float p) {
+  float seed = rand(); 
+  float sign = (seed % 2 == 0) ? -1.0 : 1.0; 
+  seed = seed / RAND_MAX; 
+  float change = seed * 0.25 * sign; 
+  return (p + change); 
+}
