b3ebd7f423fe50160d8d58a9c5e8b5b6de37ea6d
Cwalrus96 <Caleb.compton@gmail.com>
2017-11-06 19:58:15 -0600

    modified some functions

--- Project.c
+++ Project.c
@@ -7,9 +7,10 @@
 
 //Global Variables 
 ParSet * pars; //This variable will hold the array of parameter sets
-int i; 
-ParSet currentBest; 
-ParSet oldBest; 
+int i; //used for loops
+int r; // used for random variables
+ParSet * currentBest; //This variable will hold the parameter set with the highest fitness score
+ParSet * oldBest;  //This variable will hold the old parameter set with the highest fitness score
 
 https://github.com/Cwalrus96/CS-625
 //Step 1 - Initial parameters pulled from the Cobalt-Cobalt bonds in Table 1 of the paper 
@@ -64,22 +65,51 @@ int parSetComparator(const void * a, const void * b) //this function will be use
     else return 0; 
 }
 
-void rankParSets(ParSet* p) //This function will take the list of parameters and put them in order based on fitness (quickSort) 
+void rankParSets() //This function will take the list of parameters and put them in order based on fitness (quickSort) 
 {
   qsort(p, 200, sizeof(ParSet), parSetComparator); 
 }
 
-void geneticOperations() //This will coordinate and call crossover and mutate 
+ParSet * tournamentSelect() //This function will be used to select individuals for crossover
 {
-   
+    
 }
 
-void mutate() //This will use the mutation algorithm to modify the parameter set
+void geneticOperations() //This will coordinate and call crossover and mutate 
+{
+   int numMutated = 0; //Keeps track of how many parSets have been chosen for mutation. 
+   //Step 1: Reset the "mutate" attribute of all individuals to 0
+    for(i = 0; i < 100; i++) 
+    {
+        pars[i].mutate = 0.0;    
+    }
+   //Step 2: Need to randomly choose 10 individuals to undergo mutation 
+    while(numMutated < 10) 
+    {
+        r = rand(); //get a random number, then reduce it to the range 0 - 99
+        r = r % 100; 
+        if(pars[r].mutate == 0.0) //If the individual at that index hasn't been mutated yet, mutate it
+        {
+            pars[r].mutate = 1.0; //Mark that this individual has been mutated
+            pars[100 + numMutated] = mutate(&pars[r]); //add newly mutated child to the end of the array
+            numMutated ++; 
+        }
+    }
+    //Step 3: Perform crossover operation 45 times to get 90 new child individuals
+    for(i = 0; i < 45; i++) 
+    {
+        ParSet * a = tournamentSelect(); //Use tournament select to get two good individuals for crossover
+        ParSet * b = tournamentSelect(); 
+        pars[110 + (2 * i)] = crossover(a, b); //crossover will return two new individuals, add them starting at 110th index
+    }
+}
+
+ParSet * mutate() //This will use the mutation algorithm to modify the parameter set, and return the mutated individual 
 {
   
 }
 
-void crossover() //This will use the crossover algorithm to combine two parameter sets 
+ParSet * crossover(ParSet * a, ParSet * b) //This will use the crossover algorithm to combine two parameter sets, and return two child sets
 {
   
 }
