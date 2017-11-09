4e078ea6fce07d200b31e95d2d53e0b9ec328223
Cwalrus96 <Caleb.compton@gmail.com>
2017-11-09 13:21:51 -0600

    got majority of MPI version done

--- Homework5/StandardDeviation.c
+++ Homework5/StandardDeviation.c
@@ -16,9 +16,62 @@ Struct timeval t1, t2;
 Struct rusage memUsed; 
 double elapsedTime; 
 int numCores, start, end, range, rank; 
+float sum, mean; 
+float * randArray;
+float tv; 
+float iv; 
+float var; 
+float dev; 
+MPI_Status stat; 
 
 //Part 2 - Functions 
-void generateSum();
+void generateSum() //Worker processes call this function to generate their random number arrays and send the local sums to root
+{
+    sum = 0; 
+    randArray = malloc(sizeof(float) * range); 
+    for(i = 0; i < range; i++) 
+    {
+        randArray[i] = ((float) rand()); 
+        sum += randArray[i]; 
+    }
+    MPI_Send(sum, 1, MPI_FLOAT, 0, rank, MPI_COMM_WORLD);
+}
+
+void recieveSums() //root process call this function to gather all local sums 
+{
+    sum = 0;
+    float tempSum = 0; 
+    for(i = 0; i < (numCores - 1); i++) 
+    {
+        MPI_Recv(&tempSum, 1, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat); 
+        sum += tempSum
+    }
+}
+
+void getIndividualVariances()
+{
+    tv = 0; //this variable will hold the total variance
+    iv = 0; //this variable will hold the individual variance 
+    for(i = 0; i < range; i++)
+    {
+        iv = pow(randArray[i] - mean, 2); 
+        tv += iv; 
+    }
+    MPI_Send(tv, 1, MPI_FLOAT, 0, rank, MPI_COMM_WORLD); 
+}
+
+void getStandarDeviation()
+{
+    tv = 0; 
+    for(i = 0; i < (numCores - 1); i++)
+    {
+        MPI_Recv(&iv, 1, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat); 
+        tv += iv; 
+    }
+    var = tv / N; 
+    dev = sqrt(var); 
+}
+
 
 
 //Part 3 - Main Function 
@@ -35,14 +88,29 @@ int main(int argc, char ** argv)
     //Step 2 - All worker processes will randomly generate the assigned number of random numbers, and combine them together 
     if(rank != 0) 
     {
-        generateSum();
-        sendSum(); 
+        generateSum(); 
     }
-    //Step 3 - Rank 0 recieves the sum of all the randomly generated numbers from the worker processes
-    
+    //Step 3 - Rank 0 recieves the sum of all the randomly generated numbers from the worker processes, and calculates the mean
+    else 
+    {
+        recieveSums();       
+        mean = sum / N; 
+    }
     //Step 4 - After getting all the sums, Rank 0 finds the mean value and sends it to all the worker processes 
-    //Step 5 - Each worker thread uses this mean value to find their individual variants, and these are squared and added together 
+    MPI_Bcast(&mean, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    //Step 5 - Each worker thread uses this mean value to find their individual variants, and these are squared and added together
+    //These individual variances are then sent to the root process
+    if(rank != 0) 
+    {
+        getIndividualVariances(); 
+    }
     //Step 6 - Worker threads send the sums of these individual deviations to the root node, which computes the overall variance 
     //Step 7 - Finally, Root node takes the square root of the variance to get the standard deviation. 
+    else
+    {
+        getStandardDeviation();    
+    }
+    printf("Standard Deviation is %f \n", dev); 
+    printf("DATA -") //ADD TIMERS AND MEMORY MANAGEMENT DATA
 
 }
