8a60bdd0e86f191850125e7c71531528ff16df9b
Cwalrus96 <Caleb.compton@gmail.com>
2017-11-08 20:14:05 -0600

    Create StandardDeviation.c

---
+++ Homework5/StandardDeviation.c
@@ -0,0 +1,28 @@
+//Caleb Compton, Nasser Alhamadah and Hayden Svancara 
+//2017-08-11 
+//Concurrent Systems Homework 5 Part 3 - Computing Standard Deviation 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <mpi.h> 
+
+//Part 1 - Global Variables 
+int N = 1000000 //This represents how many random numbers should be generated 
+int i; 
+Struct timeval t1, t2; 
+Struct rusage memUsed; 
+
+//Part 2 - Functions 
+//Part 3 - Main Function 
+
+
+
+int main(int argc, char ** argv) 
+{
+    //Step 1 - Initialize MPI
+    //Step 2 - 
+
+}
