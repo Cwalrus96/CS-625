ab0546e2ce9458b5ef8d11a4a8440453e5f19e77
Cwalrus96 <Caleb.compton@gmail.com>
2017-12-07 17:07:27 -0600

    compiles with makefile

--- FinalProject/Project-serial.c
+++ FinalProject/Project-serial.c
@@ -24,11 +24,11 @@ struct timeval t1, t2; //used for measuring time intervals
 struct rusage memUsed;
 int mutateMax;
 float elapsedTime;
-void * lmps; 
-const float PTBCC = -275.046; 
-const float PTBCC93 = -241.4031493; 
+void * lmps;
+const float PTBCC = -275.046;
+const float PTBCC93 = -241.4031493;
 const float weight = 5.0;
-File * file; 
+FILE * file;
 
 //https://github.com/Cwalrus96/CS-625
 //Step 1 - Initial parameters pulled from the Cobalt-Cobalt bonds in Table 1 of the paper
@@ -39,116 +39,116 @@ File * file;
 
 void initialParameters() //This will create the original 100 parameter sets
 {
-    //printf("About to allocate memory for 200 ParSet Pointers \n"); 
+    //printf("About to allocate memory for 200 ParSet Pointers \n");
     pars = (ParSet**) malloc(200 * sizeof(ParSet *));
-    //printf("About to initialize each individual ParSet pointer \n"); 
-    for(i = 0; i < 200; i++) 
+    //printf("About to initialize each individual ParSet pointer \n");
+    for(i = 0; i < 200; i++)
     {
-	pars[i] = (ParSet *) malloc(sizeof(ParSet)); 
+        pars[i] = (ParSet *) malloc(sizeof(ParSet));
     }
-    //printf("About to populate first 100 parSets \n"); 
+    //printf("About to populate first 100 parSets \n");
     for(id = 0; id < 100; id++)
     {
         initializeParSet(pars[id], id);
     }
-    //printf("About to initialize the tournament array \n"); 
+    //printf("About to initialize the tournament array \n");
     tournament = (ParSet **) malloc(8 * sizeof(ParSet*));
-    //printf("About to initialize each individual in the tournament array \n"); 
-    for(i = 0; i < 8; i++) 
+    //printf("About to initialize each individual in the tournament array \n");
+    for(i = 0; i < 8; i++)
     {
-	tournament[i] = (ParSet *) malloc(sizeof(ParSet)); 
+        tournament[i] = (ParSet *) malloc(sizeof(ParSet));
     }
-    lammps_open_no_mpi(0, NULL, &lmps) //Initialize LAMMPS pointer
-}	
+    lammps_open_no_mpi(0, NULL, &lmps);  //Initialize LAMMPS pointer
+}
 
 //This file will be called to create a .tersoff file containing the parameters of an individual ParSet
 char * writeTersoffFile(ParSet * p)
 {
     //First, build file name string
     char* tersoffFile = (char *) malloc(21 * sizeof(char));  //Building a file name to hold the parameter set
-    char* idString = (char *) malloc(7 * sizeof(char)); //idString holds a string representation of the ParSet id 
-    sprintf(idString, "%d", p->id); 
-    strcat(tersoffFile, "pt"); 
-    strcat(tersoffFile, idString); 
-    strcat(tersoffFile, ".tersoff"); //tersoffFile now holds the name of a file for this individual's parameters 
-    
-    printf("%s \n", tersoffFile); 
-    
+    char* idString = (char *) malloc(7 * sizeof(char)); //idString holds a string representation of the ParSet id
+    sprintf(idString, "%f", p->id);
+    strcat(tersoffFile, "pt");
+    strcat(tersoffFile, idString);
+    strcat(tersoffFile, ".tersoff"); //tersoffFile now holds the name of a file for this individual's parameters
+
+    printf("%s \n", tersoffFile);
+
     //c, d, costheta0 (h), n, beta, lambda2, B, R, D (s), lambda1, A
     //Second, build atomic parameters string
-    char * paramString = (char *) malloc(200 * sizeof(char)); //paramString will hold the atomic parameters 
-    strcat(paramString, "Pt Pt Pt 3.0 1.0 0.0 "); 
+    char * paramString = (char *) malloc(200 * sizeof(char)); //paramString will hold the atomic parameters
+    strcat(paramString, "Pt Pt Pt 3.0 1.0 0.0 ");
     //add c to paramString
     char * param = (char *) malloc(11 * sizeof(char));
-    sprintf(param, "10%f", p->c); 
-    strcat(paramString, param); 
-    //add d    
-    sprintf(param, "10%f", p->d); 
+    sprintf(param, "10%f", p->c);
     strcat(paramString, param);
-    //add  h  
-    sprintf(param, "10%f", p->h); 
+    //add d
+    sprintf(param, "10%f", p->d);
     strcat(paramString, param);
-    //add n    
-    sprintf(param, "10%f", p->n); 
+    //add  h
+    sprintf(param, "10%f", p->h);
     strcat(paramString, param);
-    //add beta      
-    sprintf(param, "10%f", p->beta); 
-    strcat(paramString, param);    
+    //add n
+    sprintf(param, "10%f", p->n);
+    strcat(paramString, param);
+    //add beta
+    sprintf(param, "10%f", p->beta);
+    strcat(paramString, param);
     //add lambda2
-    sprintf(param, "10%f", p->lambda2); 
+    sprintf(param, "10%f", p->lambda2);
     strcat(paramString, param);
-    //add b     
-    sprintf(param, "10%f", p->b); 
-    strcat(paramString, param); 
+    //add b
+    sprintf(param, "10%f", p->b);
+    strcat(paramString, param);
     //add r
-    sprintf(param, "10%f", p->r); 
+    sprintf(param, "10%f", p->r);
     strcat(paramString, param);
     //add s
-    sprintf(param, "10%f", p->s); 
+    sprintf(param, "10%f", p->s);
     strcat(paramString, param);
     //add lambda1
-    sprintf(param, "10%f", p->lambda1); 
+    sprintf(param, "10%f", p->lambda1);
     strcat(paramString, param);
     //finally, add a
-    sprintf(param, "10%f", p->a); 
+    sprintf(param, "10%f", p->a);
     strcat(paramString, param);
-    
-    printf("%s \n", paramString); 
-    
-    //Write the parameter string to the file specified in the tersoff file string, and close the file 
-    file = fopen(tersoffFile, "w"); 
-    fputs(paramString, file); 
-    fclose(file); 
-    free(tersoffFile); 
-    free(paramString); 
-    free(idString); 
-    free(param); 
-    return 
-    
+
+    printf("%s \n", paramString);
+
+    //Write the parameter string to the file specified in the tersoff file string, and close the file
+    file = fopen(tersoffFile, "w");
+    fputs(paramString, file);
+    fclose(file);
+    free(tersoffFile);
+    free(paramString);
+    free(idString);
+    free(param);
+    return;
+
 }
 
-//Call this function to calculate the tersoff potential for an individual. 
+//Call this function to calculate the tersoff potential for an individual.
 //geo is a string that contains the name of a file. This file specifies the atomic geometry
-//paramFile is a .tersoff file that contains the parameters of a particular individual. 
-float getPotential(char * geo, char * paramFile) 
-{ 
-    printf("%s \n", geo); 
-    char* buf = (char *) malloc(sizeof(char) * 2848);	
-	
+//paramFile is a .tersoff file that contains the parameters of a particular individual.
+float getPotential(char * geo, char * paramFile)
+{
+    printf("%s \n", geo);
+    char* buf = (char *) malloc(sizeof(char) * 2848);
+
     //Use pipes to redirect stdout
     int npipe[2];
     pipe(npipe);
     int saved_stdout = dup(STDOUT_FILENO);
     dup2(npipe[1], STDOUT_FILENO);
     close(npipe[1]);
-    
+
     //Build a command string that sets up the geometry and parameters of the tersoff function for LAMMPS
     char * commandString = (char *) malloc(sizeof(char) * 350);
-    strcat(commandString, "dimension\t3\n"); 
+    strcat(commandString, "dimension\t3\n");
     strcat(commandString, "log\tnone\n");
-    strcat(commandString, "echo\tnone\n"); 
+    strcat(commandString, "echo\tnone\n");
     strcat(commandString, "boundary\tp p p\n");
-    strcat(commandString, "units\treal\n"); 
+    strcat(commandString, "units\treal\n");
     strcat(commandString, "atom_style\tcharge\n");
     strcat(commandString, "read_data\t");
     strcat(commandString, geo);
@@ -159,23 +159,23 @@ float getPotential(char * geo, char * paramFile)
     strcat(commandString, "neigh_modify\tevery 10 delay 0 check no\n");
     strcat(commandString, "minimize\t1.0e-4 1.0e-6 100 1000\n");
     strcat(commandString, "timestep\t0.25\n");
-    
-    printf("%s \n", commandString); 
-    
+
+    printf("%s \n", commandString);
+
     //Submit command to LAMMPS
     lammps_commands_string(lmps, commandString);
-    
+
     //Read standardOutput into buffer
     read(npipe[0], buf, sizeof(buf));
-    
-    //Run LAMMPS simulation 1 time 
+
+    //Run LAMMPS simulation 1 time
     lammps_command(lmps, "run\t1\n");
 
     read(npipe[0], buf, sizeof(buf));
 
     dup2(saved_stdout, STDOUT_FILENO);
-    
-    printf("%s \n", buf); 
+
+    printf("%s \n", buf);
 
     lammps_command(lmps, "clear\n");
 
@@ -183,12 +183,10 @@ float getPotential(char * geo, char * paramFile)
     ss.str(buf);
     std::string line;
     double etotal;
-
     for ( int i = 0; i < 8; i++ )
     {
         std::getline( ss, line );
     }
-
     for ( int i = 0; i < 5; i++ )
     {
          ss >> etotal;
@@ -197,27 +195,26 @@ float getPotential(char * geo, char * paramFile)
     return (rand() / RAND_MAX);
 }
 
-   
-}
+
 
 float  getFitness(ParSet * p)
 {
     //start by creating and populating a .tersoff file containing all the parameters in p
-    char * paramFile = writeTersoffFile(p); 
-    float p1 = getPotential(p, "DataPtbcc.in", paramFile); //Get potential with two different geometries and compare them. (Can expand later)
-    float p2 = getPotential(p, "DataPtbcc93.in", paramFile); 
+    char * paramFile = writeTersoffFile(p);
+    float p1 = getPotential("DataPtbcc.in", paramFile); //Get potential with two different geometries and compare them. (Can expand later)
+    float p2 = getPotential("DataPtbcc93.in", paramFile);
     float diff1 = p1 - p2;
-    float diff2 = PTBCC - PTBCC93; 
-    float diff3 = diff2 - diff1; 
-    float errorVal = pow(diff3, 2); 
-    return errorVal / weight; 
+    float diff2 = PTBCC - PTBCC93;
+    float diff3 = diff2 - diff1;
+    float errorVal = pow(diff3, 2);
+    return errorVal / weight;
 }
 
 void getFitnessAll(ParSet ** p, int setID) //This function will get the fitness scores for the specified subset of individuals
 {
     if(setID == 0) //0 means get fitness for first 100 ParSets
     {
-        //printf("Finding fitness of first 100 individuals \n"); 
+        //printf("Finding fitness of first 100 individuals \n");
         for(i = 0; i < 100; i++)
         {
             pars[i]->error = getFitness(pars[i]);
@@ -225,27 +222,27 @@ void getFitnessAll(ParSet ** p, int setID) //This function will get the fitness 
     }
     else if(setID == 1)
     {
-        //printf("Finding fitness of second 100 individuals \n"); 
+        //printf("Finding fitness of second 100 individuals \n");
         for(i = 100; i < 200; i++)
         {
             pars[i]->error = getFitness(pars[i]);
         }
     }
     else {
-	//printf("Something is wrong! We shouldn't have entered this branch!"); 
+        //printf("Something is wrong! We shouldn't have entered this branch!");
     }
 }
 
 int parSetComparator(const void * a, const void * b) //this function will be used to sort based on fitness
 {
-    ParSet ** par1 = (ParSet **) a; 
+    ParSet ** par1 = (ParSet **) a;
     ParSet ** par2 = (ParSet **) b;
-    ParSet * par3 = *par1; 
-    ParSet * par4 = *par2; 
+    ParSet * par3 = *par1;
+    ParSet * par4 = *par2;
     float x = par3->error;
-    //printf("X: %f \n", x); 
+    //printf("X: %f \n", x);
     float y = par4->error;
-    //printf("Y: %f \n", y); 
+    //printf("Y: %f \n", y);
     if(x < y) return -1;
     else if (x > y) return 1;
     else return 0;
@@ -254,39 +251,39 @@ int parSetComparator(const void * a, const void * b) //this function will be use
 //setID tells which parSets to rank - 0 is pars, 1 is tournament
 void rankParSets(ParSet ** p, int setID)
 {
-    //printf("About to rank the ParSets \n"); 
+    //printf("About to rank the ParSets \n");
     if(setID == 0)
     {
         qsort(p, 200 , sizeof(ParSet *), parSetComparator);
     }
     else if(setID == 1)
     {
-        //printf("SetID of 1 means we will be ranking the Tournament array \n"); 
+        //printf("SetID of 1 means we will be ranking the Tournament array \n");
         qsort(p, 8, sizeof(ParSet *), parSetComparator);
-	//printf("Done sorting array \n"); 
+        //printf("Done sorting array \n");
     }
 }
 
 ParSet * tournamentSelect() //This function will be used to select individuals for crossover
 {
     int numSelected = 0;
-    //printf("Selecting a tournament of 8 individuals \n"); 
+    //printf("Selecting a tournament of 8 individuals \n");
     while(numSelected < 8)  //For now, do a tournament of 8 different individuals
     {
         r = rand();
         r = r % 100;
-	//printf("choosing a random individual who hasn't yet been selected: Number %d \n", r); 
+        //printf("choosing a random individual who hasn't yet been selected: Number %d \n", r);
         if(pars[r]->selected == 0.0)
         {
             tournament[numSelected] = pars[r];
             tournament[numSelected]->selected = 1.0;
-	    numSelected++; 
+            numSelected++;
         }
-	//printf("numSelected: %d \n", numSelected); 
+        //printf("numSelected: %d \n", numSelected);
     }
-    //printf("Exitting while loop \n"); 
+    //printf("Exitting while loop \n");
     //Sort the selected individuals, and return the top 1;
-    //printf("Now about to rank the individuals \n"); 
+    //printf("Now about to rank the individuals \n");
     rankParSets(tournament,1);
     int j;
     for(j = 0; j < 8; j++)
@@ -349,7 +346,7 @@ void geneticOperations() //This will coordinate and call crossover and mutate
       //    pars[i]->mutate = 0.0;
       //}
       //Step 2: Need to randomly choose 10 individuals to undergo mutation
-      //printf("Starting mutation. Number to undergo mutation is %d \n", mutateMax); 
+      //printf("Starting mutation. Number to undergo mutation is %d \n", mutateMax);
       while(numMutated < mutateMax)
       {
         //  r = rand(); //get a random number, then reduce it to the range 0 - 99
@@ -362,15 +359,15 @@ void geneticOperations() //This will coordinate and call crossover and mutate
               numMutated ++;
         //  }
       }
-      //printf("Now moving on to crossover operation \n"); 
+      //printf("Now moving on to crossover operation \n");
       //Step 3: Perform crossover operation 45 times to get 90 new child individuals
       for(i = 0; i < (100 - mutateMax); i+=2)
       {
-      	  //printf("Selecting the first individual for crossover \n"); 
+          //printf("Selecting the first individual for crossover \n");
           ParSet * a = tournamentSelect(); //Use tournament select to get two good individuals for crossover
-	  //printf("Selecting the second individual for crossover \n"); 
+          //printf("Selecting the second individual for crossover \n");
           ParSet * b = tournamentSelect();
-	  //printf("Performing the crossover operation \n"); 
+          //printf("Performing the crossover operation \n");
           crossover(a, b); //crossover will return two new individuals, add them starting at 110th index
       }
 }
@@ -381,19 +378,19 @@ void geneticOperations() //This will coordinate and call crossover and mutate
 
 void printResults() //will be used to print the results at the end of the function
 {
-    printf("C = %f \n", pars[0]->c); 
-    printf("D = %f \n", pars[0]->d); 
-    printf("H = %f \n", pars[0]->h); 
-    printf("Beta = %f \n", pars[0]->beta); 
-    printf("Lambda2 = %f \n", pars[0]->lambda2); 
-    printf("B = %f \n", pars[0]->b); 
-    printf("R = %f \n", pars[0]->r); 
-    printf("S = %f \n", pars[0]->s); 
-    printf("Lambda1 = %f \n", pars[0]->lambda1); 
-    printf("N = %f \n", pars[0]->n); 
-    printf("A = %f \n", pars[0]->a); 
-    printf("ID = %f \n", pars[0]->id); 
-    printf("Error = %f \n", pars[0]->error); 
+    printf("C = %f \n", pars[0]->c);
+    printf("D = %f \n", pars[0]->d);
+    printf("H = %f \n", pars[0]->h);
+    printf("Beta = %f \n", pars[0]->beta);
+    printf("Lambda2 = %f \n", pars[0]->lambda2);
+    printf("B = %f \n", pars[0]->b);
+    printf("R = %f \n", pars[0]->r);
+    printf("S = %f \n", pars[0]->s);
+    printf("Lambda1 = %f \n", pars[0]->lambda1);
+    printf("N = %f \n", pars[0]->n);
+    printf("A = %f \n", pars[0]->a);
+    printf("ID = %f \n", pars[0]->id);
+    printf("Error = %f \n", pars[0]->error);
     printf("DATA, CORES, 1, TIME, %f, MEMORY, %ld \n", elapsedTime, memUsed.ru_maxrss);
 
 }
@@ -415,47 +412,48 @@ void freeAll() //this function is called at the end, and frees all global arrays
 
 int main(int argc, char** argv) {
     //Step 1. Get initial parameters
-    //printf("About to initialize parameters \n"); 
+    //printf("About to initialize parameters \n");
     initialParameters(pars); //we will make a ParSet array p, and populate it with initial parameters
-    //printf("Initial parameters initialized \n"); 
+    //printf("Initial parameters initialized \n");
     //Step 2. Enter loop
     gettimeofday(&t1, NULL);
-    //printf("Entering while loop with 1st generation \n"); 
-    currentBest = pars[1]; 
-    oldBest = pars[0]; 
-    //int numGens = 0; 
+    //printf("Entering while loop with 1st generation \n");
+    currentBest = pars[1];
+    oldBest = pars[0];
+    //int numGens = 0;
     //printf("About to get fitness for first 100 individuals \n");
     getFitnessAll(pars,0);
     while((currentBest != oldBest) )//&& (numGens < 100))
     {
-	//printf("About to perform genetic operations \n"); 
+        //printf("About to perform genetic operations \n");
         geneticOperations();
-	//printf("About to calculate fitness for newly generated individuals \n"); 
+        //printf("About to calculate fitness for newly generated individuals \n");
         getFitnessAll(pars, 1);
-	//printf("About to rank the parSets \n"); 
+        //printf("About to rank the parSets \n");
         rankParSets(pars,0);
-	//printf("Checking for convergence \n"); 
+        //printf("Checking for convergence \n");
         if(currentBest != NULL)
         {
             oldBest = currentBest;
         }
         currentBest = pars[0];
-	printf("OldBest = %f, %f, newBest = %f, %f \n", oldBest->error, oldBest->id, currentBest->error, currentBest->id);
-	
-//	numGens ++; 
+        printf("OldBest = %f, %f, newBest = %f, %f \n", oldBest->error, oldBest->id, currentBest->error, currentBest->id);
+
+//      numGens ++;
     }
-    //printf("Exitting while loop \n"); 
+    //printf("Exitting while loop \n");
     //simplex();
     gettimeofday(&t2, NULL);
     elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
     elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
     getrusage(RUSAGE_SELF, &memUsed);
-    //printf("Printing results \n"); 
+    //printf("Printing results \n");
     printResults();
-    //printf("Freeing allocated memory \n"); 
+    //printf("Freeing allocated memory \n");
     freeAll();
     //printf("Exitting");
     return 0;
 }
+
 
 
