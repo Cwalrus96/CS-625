5cc8dba50a2bed4e27356145cf64a5deec062694
Cwalrus96 <Caleb.compton@gmail.com>
2017-12-09 21:51:54 -0600

    Update ParSet.h

--- FinalProject/ParSet.h
+++ FinalProject/ParSet.h
@@ -4,26 +4,26 @@
 #include <stdlib.h>
 
 
-typedef Struct parSet { ///This structure will be used to hold individual parameter sets
-  float error; 
-  float id; 
-  float c; 
-  float d; 
-  float h; 
-  float beta; 
-  float n; 
-  float lambda2; 
-  float b; 
-  float r; 
-  float s; 
-  float lambda1; 
-  float a; 
-  float mutate = 0.0;  //keeps track if this individual is selected for mutation
-  float selected = 0.0; //keeps track if this individual is selected for tournament select 
-}ParSet; 
+typedef struct parSet { ///This structure will be used to hold individual parameter sets
+  float error;
+  float id;
+  float c;
+  float d;
+  float h;
+  float beta;
+  float n;
+  float lambda2;
+  float b;
+  float r;
+  float s;
+  float lambda1;
+  float a;
+  float mutate;  //keeps track if this individual is selected for mutation
+  float selected; //keeps track if this individual is selected for tournament select
+}ParSet;
 
 
-initializeParSet(*ParSet p, int id); //This function will take a ParSet pointer and initialize that structure with randomized values
+void initializeParSet(ParSet *  p, int id); //This function will take a ParSet pointer and initialize that structure with randomized values
 
-randomizeParameter(float p); //This function will return a parameter that has randomly been adjusted in the range of +/-25%
-
+float randomizeParameter(float p); //This function will return a parameter that has randomly been adjusted in the range of +/-25%
+#endif
